9809debd654972068f4a0ab3bab0390d6599fa09
Kostyantyn Stepanyuk <kostya.stepanyuk@gmail.com>
2013-12-02 14:25:16 +0200

    Fix user's tests

--- test/user.js
+++ test/user.js
@@ -12,8 +12,8 @@ describe('User', function() {
   describe('getUser', function() {
     before(function() {
       nock('http://localhost:8000').get('/users/1.json?pio_appkey=key').reply(200, {uid: 1});
-      nock('http://localhost:8000').get('/users/2.json?pio_appkey=key').reply(400, {errors: {field: 'pio_appkey', message: 'This field is required'}});
-      nock('http://localhost:8000').get('/users/3.json?pio_appkey=key').reply(400, {error: {message: 'Cannot find user.'}});
+      nock('http://localhost:8000').get('/users/2.json?pio_appkey=key').reply(400, {errors: [{field: 'pio_appkey', message: 'This field is required'}]});
+      nock('http://localhost:8000').get('/users/3.json?pio_appkey=key').reply(404, {message: 'Cannot find user.'});
     });
 
     it('should return user object', function(done) {
@@ -25,14 +25,14 @@ describe('User', function() {
 
     it('should return error message if pio_appkey is missed', function(done) {
       prediction.user.getUser(2, function(err, data) {
-        assert_objects(data, {errors: {field: 'pio_appkey', message: 'This field is required'}});
+        assert_objects(data, {errors: [{field: 'pio_appkey', message: 'This field is required'}]});
         done();
       });
     });
 
     it('should return error message if cannot find user by ID', function(done) {
       prediction.user.getUser(3, function(err, data) {
-        assert_objects(data, {error: {message: 'Cannot find user.'}});
+        assert_objects(data, {message: 'Cannot find user.'});
         done();
       });
     });
@@ -41,7 +41,7 @@ describe('User', function() {
   describe('createUser', function() {
     before(function() {
       nock('http://localhost:8000').post('/users.json', {pio_appkey: 'key', pio_uid: 1}).reply(201, {message: 'User created.'});
-      nock('http://localhost:8000').post('/users.json', {pio_appkey: 'key', pio_uid: 2}).reply(400, {errors: {field: 'pio_appkey', message: 'This field is required'}});
+      nock('http://localhost:8000').post('/users.json', {pio_appkey: 'key', pio_uid: 2}).reply(400, {errors: [{field: 'pio_appkey', message: 'This field is required'}]});
     });
 
     it('should return created user object', function(done) {
@@ -53,7 +53,7 @@ describe('User', function() {
 
     it('should return error message if pio_appkey is missed', function(done) {
       prediction.user.createUser(2, function(err, data) {
-        assert_objects(data, {errors: {field: 'pio_appkey', message: 'This field is required'}});
+        assert_objects(data, {errors: [{field: 'pio_appkey', message: 'This field is required'}]});
         done();
       });
     });
@@ -62,7 +62,7 @@ describe('User', function() {
   describe('deleteUser', function() {
     before(function() {
       nock('http://localhost:8000').delete('/users/1.json?pio_appkey=key').reply(200, {message: 'User deleted.'});
-      nock('http://localhost:8000').delete('/users/2.json?pio_appkey=key').reply(400, {errors: {field: 'pio_appkey', message: 'This field is required'}});
+      nock('http://localhost:8000').delete('/users/2.json?pio_appkey=key').reply(400, {errors: [{field: 'pio_appkey', message: 'This field is required'}]});
     });
 
     it('should return created user object', function(done) {
@@ -74,7 +74,7 @@ describe('User', function() {
 
     it('should return error message if pio_appkey is missed', function(done) {
       prediction.user.deleteUser(2, function(err, data) {
-        assert_objects(data, {errors: {field: 'pio_appkey', message: 'This field is required'}});
+        assert_objects(data, {errors: [{field: 'pio_appkey', message: 'This field is required'}]});
         done();
       });
     });
